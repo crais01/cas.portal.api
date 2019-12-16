3cdbb2c7b773c7b2d62184e5de1c197bcb113dcf
crais01 <ccortesj.01@gmail.com>
2019-12-16 16:23:47 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 session_start();   
 
-// Instantiate the app  htrh gf
+// Instantiate the app
 $settings = require __DIR__ . '/../src/settings.php';
 $app = new \Slim\App($settings);
 
